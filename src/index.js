1c5bdc5ec7b15d540ff4f2438fb117b7c9b5c6a9
Owoyemi Olusola <olusola.owoyemi@cyberspace.net.ng>
2024-06-12 10:09:28 +0100

    initiate

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ const verifyToken = require('./middleware/verifyToken')
 // const Event = require('./models/event');
 // const sequelize = require('./models/index');
 
+const bcrypt = require('bcrypt');
+
+const password = 'Oluseyi@123';
+const saltRounds = 10;
+
+bcrypt.hash(password, saltRounds, function(err, hash) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Hashed Password:', hash);
+  }
+});
+
 
 
 
